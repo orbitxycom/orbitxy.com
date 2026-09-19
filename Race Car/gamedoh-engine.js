57154dfe7e90f9f510129fa4cf2f8aa259584bef
orbitxycom <orbitxy.com@gmail.com>
2026-09-19 14:33:05 +0700

    Create gamedoh-engine.js

---
+++ Race Car/gamedoh-engine.js
@@ -0,0 +1,47 @@
+import BaseGame from 'https://jskidz.au/gamedoh-engine/v1/Game.js';
+import Button from 'https://jskidz.au/gamedoh-engine/v1/Button.js';
+import Countdown from 'https://jskidz.au/gamedoh-engine/v1/Countdown.js';
+import GameTimer from 'https://jskidz.au/gamedoh-engine/v1/GameTimer.js';
+import Image from 'https://jskidz.au/gamedoh-engine/v1/Image.js';
+import Level from 'https://jskidz.au/gamedoh-engine/v1/Level.js';
+import LevelIntro from 'https://jskidz.au/gamedoh-engine/v1/LevelIntro.js';
+import PixelButton from 'https://jskidz.au/gamedoh-engine/v1/PixelButton.js';
+import ScoreBoard from 'https://jskidz.au/gamedoh-engine/v1/ScoreBoard.js';
+import SoundManager from 'https://jskidz.au/gamedoh-engine/v1/SoundManager.js';
+import State from 'https://jskidz.au/gamedoh-engine/v1/State.js';
+import Timer from 'https://jskidz.au/gamedoh-engine/v1/Timer.js';
+import { GAME_STATE } from 'https://jskidz.au/gamedoh-engine/v1/constants.js';
+import {
+  drawGrid,
+  drawShape,
+  isCollision,
+  getRandomNumber,
+  shuffle,
+  delay,
+  stopAllAudio,
+  Shake,
+} from 'https://jskidz.au/gamedoh-engine/v1/utils.js';
+
+export {
+  BaseGame,
+  Button,
+  Countdown,
+  GameTimer,
+  Image,
+  Level,
+  LevelIntro,
+  PixelButton,
+  ScoreBoard,
+  SoundManager,
+  State,
+  Timer,
+  GAME_STATE,
+  drawGrid,
+  drawShape,
+  isCollision,
+  getRandomNumber,
+  shuffle,
+  delay,
+  stopAllAudio,
+  Shake,
+};
